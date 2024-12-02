ffb5c41ab369cf5e3908de47d72966627c80f19a
ErneDainesi <edainesi@fi.uba.ar>
2024-12-02 19:07:39 -0300

    day 1 done

--- day01/day01.go
+++ day01/day01.go
@@ -1,7 +1,80 @@
 package day01
 
-func PartOne(input string) (int, error) {
-    return 12, nil
+import (
+	"bytes"
+	"sort"
+	"strconv"
+)
+
+func PartOne(input []byte) int {
+    var left []int
+    var right []int
+    appendLeft := true
+    var buffer bytes.Buffer
+    for _, char := range input {
+        if char != 32 && int64(char) != 10 {
+            buffer.WriteString(string(char))
+        } else {
+            num, _ := strconv.Atoi(buffer.String())
+            if appendLeft {
+                left = append(left, num)
+            } else {
+                right = append(right, num)
+            }
+            appendLeft = !appendLeft
+            buffer.Reset()
+        }
+    }
+    sort.Sort(sort.IntSlice(left))
+    sort.Sort(sort.IntSlice(right))
+    result := 0
+    for pos, val := range left {
+        partial := val - right[pos]
+        if partial < 0 {
+            partial *= -1
+        }
+        result += partial
+    }
+    return result
 }
 
-func PartTwo() {}
+func PartTwo(input string) int {
+    var left []int
+    var right []int
+    appendLeft := true
+    var buffer bytes.Buffer
+    for _, char := range input {
+        if char != 32 && int64(char) != 10 {
+            buffer.WriteString(string(char))
+        } else {
+            num, _ := strconv.Atoi(buffer.String())
+            if (num > 0) {
+                if appendLeft {
+                    left = append(left, num)
+                } else {
+                    right = append(right, num)
+                }
+                appendLeft = !appendLeft
+                buffer.Reset()
+            }
+        }
+    }
+    m := make(map[int]int)
+    for _, num := range left {
+        if _, ok := m[num]; !ok {
+            m[num] = 0
+        }
+    }
+    for _, num := range right {
+        if reps, ok := m[num]; ok {
+            m[num] = reps + 1
+        }
+    }
+    result := 0
+    for _, num := range left {
+        if reps, ok := m[num]; ok {
+            result += num * reps
+        }
+    }
+    return result
+}
--- day01/day01_test.go
+++ day01/day01_test.go
@@ -6,34 +6,26 @@ import (
 )
 
 func TestPartOne(t *testing.T) {
-    expectedValue := 11
+    expectedValue := 2815556
     input, error := os.ReadFile("input.txt")
     if error != nil {
         t.Fatalf("error while reading file")
     }
-    result, error := PartOne(string(input))
-    if error != nil {
-        t.Fatalf("WRONG!")
-    }
-
+    result := PartOne(input)
     if result != expectedValue {
-        t.Fatalf("WRONG!")
+        t.Fatalf("[PART 1] WRONG! got %d expected %d", result, expectedValue)
     }
 }
 
 
 func TestPartTwo(t *testing.T) {
-    expectedValue := 11
+    expectedValue := 23927637
     input, error := os.ReadFile("input2.txt")
     if error != nil {
         t.Fatalf("error while reading file")
     }
-    result, error := PartOne(string(input))
-    if error != nil {
-        t.Fatalf("WRONG!")
-    }
-
+    result := PartTwo(string(input))
     if result != expectedValue {
-        t.Fatalf("WRONG!")
+        t.Fatalf("[PART 2] WRONG! got %d expected %d", result, expectedValue)
     }
 }
